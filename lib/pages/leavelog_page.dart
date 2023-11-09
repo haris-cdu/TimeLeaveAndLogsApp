a4b6734b1049e79f65e4086abf9b2fc8a259aded
Haris-cdu <mohammadharis.cdu@gmail.com>
2023-11-09 11:14:29 +0530

    Added working logs and leave logs

--- lib/pages/leavelog_page.dart
+++ lib/pages/leavelog_page.dart
@@ -1,14 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../constants/functions.dart';
+import '../constants/responsive.dart';
+import '../provider/graphdata_provider.dart';
 
 class LeaveLogPage extends StatelessWidget {
   const LeaveLogPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: const Center(
-        child: Text("LeaveLog page"),
-      ),
+    return Consumer<GraphDataProvider>(
+      builder: (context, provider, child) => provider.leaveLog.isEmpty
+          ? Center(
+              child: Text(
+              "No leave logs!",
+              style: TextStyle(
+                  fontSize: dp(context, 20), fontWeight: FontWeight.bold),
+            ))
+          : ListView.builder(
+              itemCount: provider.leaveLog.length,
+              itemBuilder: (context, index) => Card(
+                child: ExpansionTile(
+                    title: Text(
+                      formatWorkLogDate(provider.leaveLog[index].date),
+                      style: const TextStyle(fontWeight: FontWeight.w500),
+                    ),
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 5.0, horizontal: 15),
+                        child: Align(
+                          alignment: Alignment.centerLeft,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Time taken: ${provider.leaveLog[index].totalTime}",
+                                style: TextStyle(
+                                  fontSize: dp(context, 16),
+                                ),
+                              ),
+                              Text(
+                                "Reason: ${provider.leaveLog[index].reason}",
+                                style: TextStyle(
+                                  fontSize: dp(context, 16),
+                                ),
+                              ),
+                              Text(
+                                "Leave-type: ${provider.leaveLog[index].type}",
+                                style: TextStyle(
+                                  fontSize: dp(context, 16),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ]),
+              ),
+            ),
     );
   }
 }
--- lib/pages/worklog_page.dart
+++ lib/pages/worklog_page.dart
@@ -1,4 +1,5 @@
 import 'package:employee_timesheet/constants/functions.dart';
+import 'package:employee_timesheet/constants/responsive.dart';
 import 'package:employee_timesheet/provider/graphdata_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -9,38 +10,60 @@ class WorkLogPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Consumer<GraphDataProvider>(
-      builder: (context, provider, child) => ListView.builder(
-        // physics: const NeverScrollableScrollPhysics(),
-        shrinkWrap: true,
-        itemCount: provider.workingLog.length,
-        itemBuilder: (context, index) => Card(
-          child: ExpansionTile(
-              title: Text(
-                formatWorkLogDate(provider.workingLog[index].date),
-                style: const TextStyle(fontWeight: FontWeight.w500),
-              ),
-              children: [
-                ListView.builder(
-                  itemCount: provider.workingLog[index].projectLog.length,
-                  itemBuilder: (context, logIndex) => Column(
+      builder: (context, provider, child) => provider.workingLog.isEmpty
+          ? Center(
+              child: Text(
+              "No working logs!",
+              style: TextStyle(
+                  fontSize: dp(context, 20), fontWeight: FontWeight.bold),
+            ))
+          : ListView.builder(
+              itemCount: provider.workingLog.length,
+              itemBuilder: (context, index) => Card(
+                child: ExpansionTile(
+                    title: Text(
+                      formatWorkLogDate(provider.workingLog[index].date),
+                      style: const TextStyle(fontWeight: FontWeight.w500),
+                    ),
                     children: [
-                      Text(provider
-                          .workingLog[index].projectLog[logIndex].projectName),
                       ListView.builder(
-                        itemCount: provider.workingLog[index]
-                            .projectLog[logIndex].tasks.length,
-                        itemBuilder: (context, taskIndex) => Text(provider
-                            .workingLog[index]
-                            .projectLog[logIndex]
-                            .tasks[taskIndex]
-                            .taskName),
+                        physics: const NeverScrollableScrollPhysics(),
+                        shrinkWrap: true,
+                        itemCount: provider.workingLog[index].projectLog.length,
+                        itemBuilder: (context, logIndex) => Padding(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 5.0, horizontal: 15),
+                          child: Column(
+                            children: [
+                              Align(
+                                alignment: Alignment.centerLeft,
+                                child: Text(
+                                  provider.workingLog[index]
+                                      .projectLog[logIndex].projectName,
+                                  style: TextStyle(
+                                      fontSize: dp(context, 16),
+                                      fontWeight: FontWeight.w500),
+                                ),
+                              ),
+                              ListView.builder(
+                                physics: const NeverScrollableScrollPhysics(),
+                                shrinkWrap: true,
+                                itemCount: provider.workingLog[index]
+                                    .projectLog[logIndex].tasks.length,
+                                itemBuilder: (context, taskIndex) => Padding(
+                                  padding: const EdgeInsets.only(left: 8.0),
+                                  child: Text(
+                                    "-> ${provider.workingLog[index].projectLog[logIndex].tasks[taskIndex].taskName}",
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
                       )
-                    ],
-                  ),
-                )
-              ]),
-        ),
-      ),
+                    ]),
+              ),
+            ),
     );
   }
 }
